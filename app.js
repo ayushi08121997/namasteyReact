0a59aa850c472b67ab525a4cede1f119df845cff
Ayushi <ashrivastava0812@gmail.com>
2025-03-25 07:38:17 +0530

    first commit

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
-const heading = React.createElement("h1",
-{ 
-    id : "heading"
-},"hello world!!");
-console.log(heading)
+const parent = React.createElement("div",{id : "parent"},
+    React.createElement("div",{id:"child"},
+       React.createElement("h1",null,"I am an h1 tag!!")
+    )
+);
+// const heading = React.createElement("h1",
+// { 
+//     id : "heading"
+// },"hello world!!");
+console.log(parent)
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(parent);
